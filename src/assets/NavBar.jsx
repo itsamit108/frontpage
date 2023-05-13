d007d6af2473eda15559e26f3031e9b79e24bba3
itsamit108 <itsamit108@outlook.com>
2023-05-13 19:12:22 +0530

    update NavBar.jsx

--- src/assets/NavBar.jsx
+++ src/assets/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{ backgroundColor: '#3339cd' }}>
                 <div className="container-fluid">
                     <Link className="navbar-brand navbar-dark text-white" to="/">
-                        <h3>FrontPage</h3>
+                        <h4>FrontPage</h4>
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
